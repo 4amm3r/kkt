bb43a676a22401c9ab5f1dfccce3235f838fa5b9
Nadia <nadia@kittykattime.com>
2017-08-24 23:19:14 -0700

    no message

--- SiteNewImproved/js/contact_me.js
+++ SiteNewImproved/js/contact_me.js
@@ -47,7 +47,7 @@ $(function() {
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Your message has been sent. Please check your mailbox for a confirmation email.</strong>");
                     $('#success > .alert-success')
                         .append('</div>');
 
