6286f6ecac3e7737325c357e15fd67afc7c06ffd
Nadia <nadia@kittykattime.com>
2017-09-05 20:50:34 -0700

    Updated to new template and look

---
+++ js/agency.js
@@ -0,0 +1,71 @@
+// Agency Theme JavaScript
+
+(function($) {
+    "use strict"; // Start of use strict
+
+    // jQuery for page scrolling feature - requires jQuery Easing plugin
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+    // Highlight the top nav as scrolling occurs
+    $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+    });
+
+    // Closes the Responsive Menu on Menu Item Click
+    $('.navbar-collapse ul li a').click(function(){ 
+            $('.navbar-toggle:visible').click();
+    });
+
+    // Offset for Main Navigation
+    $('#mainNav').affix({
+        offset: {
+            top: 100
+        }
+    })
+    
+    var recommendView = false;
+    $('#recommendationdiv').hide();
+    $('#recommendexpand').on('click', function(){
+        if(recommendView == false){                   
+            $('#recommendexpand').text('See Less');
+            $('#recommendationdiv').slideDown(1000);
+        }
+        else{                                       
+            $('#recommendexpand').text('See More');
+            $('#recommendationdiv').slideUp(1000);
+        }
+        recommendView = !recommendView;
+    });
+    
+    var testimonialView = false;
+    $('#testimonialdiv').hide();
+    $('#testimonialexpand').on('click', function(){
+        if(testimonialView == false){                   
+            $('#testimonialexpand').text('See Less');
+            $('#testimonialdiv').slideDown(1000);
+        }
+        else{                                       
+            $('#testimonialexpand').text('See More');
+            $('#testimonialdiv').slideUp(1000);
+        }
+        testimonialView = !testimonialView;
+    });
+    
+    $('#privacypolicy').on('click', function(){
+        var url = "../privacypolicy.html";
+        var options = {
+            url: url,
+            title: 'Privacy Policy',
+            size: eModal.size.lg
+        };
+        eModal.ajax(options);
+    });
+
+})(jQuery); // End of use strict
---
+++ mail/contact_me.php
@@ -0,0 +1,78 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$street = strip_tags(htmlspecialchars($_POST['street']));
+$city = strip_tags(htmlspecialchars($_POST['city']));
+$zip = strip_tags(htmlspecialchars($_POST['zip']));
+$startdate = strip_tags(htmlspecialchars($_POST['startdate']));
+$enddate = strip_tags(htmlspecialchars($_POST['enddate']));
+$servicesrequested = strip_tags(htmlspecialchars($_POST['servicesrequested']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = 'nadia@kittykattime.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "New Kitty Kat Time Request";
+$addressline = "";
+$dateline = "";
+$serviceline = "";
+$messageline = "";
+
+if($street !== "" || $city !== "" || $zip !== ""){
+    $addressline = "Address: ";
+}
+if($street !== ""){
+    $addressline .= $street;
+    $addressline .= "\n";
+}
+if($city !== ""){
+    $addressline .= $city;
+    $addressline .= ", WA ";
+}
+if($zip !== ""){
+    $addressline .= $zip;
+    $addressline .= "\n\n";
+}
+else{
+    $addressline .= "\n\n";
+}
+if($startdate !== "" || $enddate !== ""){
+    $dateline = "Dates: ";
+}
+if($startdate !== ""){
+    $dateline .= $startdate;
+    $dateline .= " to ";
+}
+if($enddate !== ""){
+    $dateline .= $enddate;
+    $dateline .= "\n\n";
+}
+else if($startdate !== ""){
+    $dateline .= "\n\n";
+}
+if($servicesrequested !== ""){
+    $serviceline = "Service: ";
+    $serviceline .= $servicesrequested;
+    $serviceline .= "\n\n";
+}
+if($message !== ""){
+    $messageline = "Message: ";
+    $messageline .= $message;
+}
+
+$email_body = "Name: $name\n\nEmail: $email_address\n\nPhone: $phone\n\n$addressline$dateline$serviceline$messageline";
+$headers = "From: nadia@kittykattime.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+return true;         
+?>
